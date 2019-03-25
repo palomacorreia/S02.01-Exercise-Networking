07ff1b76a4c49d06b2bce48001063845b6706db3
Paloma02 <palomacorreiadasilva@gmail.com>
2019-03-25 15:14:00 -0300

    network

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,42 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    //COMPLETED TODO (1) Create a field to store the weather display TextView
+
+    TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
-
-        // TODO (3) Create an array of Strings that contain fake weather data
-
+        //COMPLETED  TODO (2) Use findViewById to get a reference to the weather display TextView
+            display= (TextView)findViewById(R.id.tv_weather_data);
+        // COMPLETED TODO (3) Create an array of Strings that contain fake weather data
+        String [] dummyWeatherData = {
+                " Hoje, 17 de maio - Claro - 17 ° C / 15 ° C " ,
+                " Amanhã - Nublado - 19 ° C / 15 ° C " ,
+                " Quinta-feira - chuvosa - 30 ° C / 11 ° C " ,
+                " Sexta-feira - trovoadas - 21 ° C / 9 ° C " ,
+                " Sábado - Trovoadas - 16 ° C / 7 ° C " ,
+                " Domingo - Chuvoso - 16 ° C / 8 ° C " ,
+                " Segunda-feira - Parcialmente nublado - 15 ° C / 10 ° C " ,
+                " Ter, 24 de maio - almôndegas - 16 ° C / 18 ° C " ,
+                " Qua, 25 de maio - Nublado - 19 ° C / 15 ° C " ,
+                " Qui, 26 de maio - Tempestuoso - 30 ° C / 11 ° C " ,
+                " Sexta, 27 de maio - Furacão - 21 ° C / 9 ° C " ,
+                " Sábado, 28 de maio - Meteoros - 16 ° C / 7 ° C " ,
+                " Sun, 29 de maio - Apocalypse - 16 ° C / 8 ° C " ,
+                " Seg, 30 de maio - pós Apocalipse - 15 ° C / 10 ° C " ,
+        };
         // TODO (4) Append each String from the fake weather data array to the TextView
+        for (String dummyWeatherDay : dummyWeatherData) {
+           display.append(dummyWeatherDay + "\n\n\n");
+        }
     }
+ 
 }
